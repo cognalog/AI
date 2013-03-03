d4841d38caf1be5bc3eabbe368cb60aa72f9fe09
Tyrone Hinderson, Jr <tph2107@columbia.edu>
2013-03-03 17:06:53 -0500

    moved bfs file

--- ass2/bfs.lisp
+++ ass2/bfs.lisp
@@ -12,6 +12,9 @@
 (defun push-back (e ls)
   (append ls `(,e)))
 
+(defun zero (n)
+  (eq n 0))
+
 (defun solution (n)
   (cond
     ((null n) nil)
@@ -22,27 +25,30 @@
     ((equal e (car ls)) 0)
     (T (+ 1 (pos e (cdr ls))))))
 
+(defun get-0-x (state) (cadr (getl (- (length state) 1) state)))
+(defun get-0-y (state) (car (getl (- (length state) 1) state)))
+
 (defun getl (n ls)
   (cond
-    ((eq n 0) (car ls))
+    ((zero n) (car ls))
     (T (getl (- n 1) (cdr ls)))))
 
 (defun setl (n ls e)
   (cond
     ((null ls) ls)
-    ((eq n 0) (cons e (setl (- n 1) (cdr ls) e)))
+    ((zero n) (cons e (setl (- n 1) (cdr ls) e)))
     (T (cons (car ls) (setl (- n 1) (cdr ls) e)))))
 
 (defun grid-get (x y grid)
   (cond
     ((null grid) grid)
-    ((eq y 0) (getl x (car grid)))
+    ((zero y) (getl x (car grid)))
     (T (grid-get x (- y 1) (cdr grid)))))
 
 (defun grid-set (x y grid e)
     (cond
       ((null grid) grid)
-      ((eq y 0) (cons (setl x (car grid) e) (grid-set x (- y 1) (cdr grid) e)))
+      ((zero y) (cons (setl x (car grid) e) (grid-set x (- y 1) (cdr grid) e)))
       (T (cons (car grid) (grid-set x (- y 1) (cdr grid) e)))))
 
 (defun succ-fxn (node ops)
@@ -51,52 +57,44 @@
        do
 	 (setf s-states (funcall op n-state))
 	 (setf s-nodes 
-	       (apend s-nodes
-		      (mapcar (lambda (s-state) `(,s-state ,op ,node)) s-states)))
+	       (append s-nodes
+		      (mapcar (lambda (s-state) `(,s-state ,op ,node)) s-states))))
     s-nodes))
 
-(defun blank-up (parent)
-  (let ((par parent) (child '()))
-    (loop for row in par
-       for y from 0 to  (length par)
-       do
-	 (if (member nil row)
-	     (if (eq y 0) ;can't go further up
-		 (return-from blank-up nil)
-		 (setf child 
-		       (push-back ; rows 2+ may need modification
-			(loop for col in row
-			   for x from 0 to (length row)
-			   collect
-			     (if (null col)
-				 (progn
-				   (setf child (grid-set x (- y 1) child col))
-				   (grid-get x (- y 1) par)) ;swap
-				 col)) ;no modification
-			child)))
-	     (setf child (push-back row child))))
-       child))
+;OPERATOR FUNCTIONS FOR N-PUZZLE
 
-(defun blank-left (parent)
-  (let ((par parent) (child '()))
-    (loop for row in par
-       for y from 0 to  (length par)
-       do
-	 (if (member nil row)
-	     (setf child 
-		   (push-back ; rows 2+ may need modification
-		    (loop for col in row
-		       for x from 0 to (length row)
-		       collect
-			 (if (null col)
-			     (progn
-			       (if (eq x 0) (return-from blank-left nil)) ;can't go further left
-			       (setf child (grid-set (- x 1) y par col))
-			       (grid-get (- x 1) y par)) ;swap
-			     col)) ;no modification
-		    child))
-	     (setf child (push-back row child))))
-       child))
+(defun north (parent)
+  (let ((x (get-0-x parent)) (y (get-0-y parent)) (child parent))
+    (if (zero y) (return-from north nil))
+    (setf child (grid-set x y child (grid-get x (- y 1) child)))
+    (setf child (grid-set x (- y 1) child 0)) ;no modification
+    (setf (car (getl (- (length child) 1) child)) (- y 1))
+    child))
+
+(defun west (parent)
+  (let ((x (get-0-x parent)) (y (get-0-y parent)) (child parent))
+    (if (zero x) (return-from west nil))
+    (setf child (grid-set x y child (grid-get (- x 1) y child)))
+    (setf child (grid-set (- x 1) y child 0)) ;no modification
+    (setf (cadr (getl (- (length child) 1) child)) (- x 1))
+    child))
+
+(defun south (parent)
+  (let ((x (get-0-x parent)) (y (get-0-y parent)) (child parent))
+    (if (eq y (- (length child) 2)) (return-from south nil))
+    (setf child (grid-set x y child (grid-get x (+ y 1) child)))
+    (setf child (grid-set x (+ y 1) child 0)) ;no modification
+    (setf (car (getl (- (length child) 1) child)) (+ y 1))
+    child))
+
+
+(defun east (parent)
+  (let ((x (get-0-x parent)) (y (get-0-y parent)) (child parent))
+    (if (eq x (- (length (car child)) 1)) (return-from east nil))
+    (setf child (grid-set x y child (grid-get (+ x 1) y child)))
+    (setf child (grid-set (+ x 1) y child 0)) ;no modification
+    (setf (cadr (getl (- (length child) 1) child)) (+ x 1))
+    child))
 
 (defun bfs (s0 sg kids)
   (let ((ahead `((,s0 ,nil ,nil))) ;list of unexplored nodes, starting with s0
@@ -110,7 +108,7 @@
        (if (equal (state n) sg) ;if goal state has been reached
 	   (print "Solution:")
 	   (return (solution n)))
-       (setf children (funcall kids n `(,#'blank-up)) ;collect n's child nodes into a list
+       (setf children (funcall kids n `(,#'north ,#'south ,#'east ,#'west)) ;collect n's child nodes into a list
        (setf children ;get rid of nodes we already have/will cover
 	     (diff children (append ahead behind)))
        (setf ahead (append ahead children)))))) ;add newly generated nodes to frontier

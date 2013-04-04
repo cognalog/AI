2b97304f7706ae9e8a2bd341022e909795e044d9
Tyrone Hinderson, Jr <tph2107@columbia.edu>
2013-04-04 01:09:00 -0400

    began writeup, done with heuristics

---
+++ 3-ass/README.txt
@@ -0,0 +1,8 @@
+Tyrone Hinderson, tph2107
+
+My evaluation function
+   -determines the number of moves available to me, as well as the number of those available to my opponent.
+   -looks at how clumpy the asterices are
+   -promotes development of whole lines or rows full of asterices
+   	-so that I end up on the bigger side of the partition
+   -promotes maintainence of a large area of open space
--- 3-ass/isolation.lisp
+++ 3-ass/isolation.lisp
@@ -21,6 +21,7 @@
     ((equal p 'x) (second state))
     ((equal p 'o) (third state))
     (T nil)))
+(defun opponent (pl) (if (equal pl 'x) 'o 'x))
 (defun previous (state) (fourth state))
 (defun p-row (player) (second player))
 (defun p-col (player) (third player))
@@ -133,7 +134,7 @@
 (defun relative-flex (pl state)
   (- 
    (* (length (possible-moves pl state)) 2)
-   (length (possible-moves (if (equal pl 'x) 'o 'x)  state))))
+   (length (possible-moves (opponent pl) state))))
 
 ;these two determine the amount of adjacency among asterices
 (defun adjacent-asterices (row col state)
@@ -153,17 +154,85 @@
 				(adjacent-asterices r c state)
 				0)))))))    
 
-(defun utility (pl state)
-  (let ((opp (if (equal pl 'x) 'o 'x)) (moves (length (possible-moves pl state))))
+;find the area of freedom
+(defun row-ast (row)
+  (cond
+    ((null row) 0)
+    ((equal (car row) '*) (+ 1 (row-ast (cdr row))))
+    (t (row-ast (cdr row)))))
+
+(defun col-ast (col board)
+  (cond
+    ((null board) 0)
+    ((equal (getl col (car board)) '*)
+     (+ 1 (col-ast col (cdr board))))
+    (t (col-ast col (cdr board)))))
+
+(defun free-area (pl state)
+  (let ((p-r (p-row (player pl state)))
+	(p-c (p-col (player pl state)))
+	(grid (board state))
+	(n-bound 0) (s-bound (length (board state)))
+	(e-bound (length (car (board state)))) (w-bound 0))
+;define perimeter based on asterisk density
+    (loop for r in grid
+       for i from 1 to (length grid) do
+	 (if (< i p-r)
+	     (if (< (row-ast (getl n-bound grid))
+		    (row-ast r))
+		 (setf n-bound i)))
+	 (if (> i p-r)
+	     (if (< (row-ast (getl s-bound grid))
+		    (row-ast r))
+		 (setf s-bound i))))
+    (loop for j from 1 to (length (car grid)) do
+	 (if (< j p-c)
+	     (if (< (col-ast w-bound grid)
+		    (col-ast j grid))
+		 (setf w-bound j)))
+	 (if (> j p-c)
+	     (if (< (col-ast e-bound grid)
+		    (col-ast j grid))
+		 (setf e-bound j))))
+    (* (- s-bound n-bound)
+       (- e-bound w-bound))))
+    
+(defun relative-freedom (pl state)
+  (- (* (free-area pl state) 1.5)
+     (free-area (opponent pl) state)))
+
+(defun casual-ev (pl state)
+  (let ((opp (opponent pl)) (moves (length (possible-moves pl state))))
     (cond
       ((eq moves 0) -999)
       ((eq (length (possible-moves opp state)) 0) 999)
-      (t (+ (relative-flex pl state)
-	    (/ (asterisk-parties state) 4))))))
+      (t (+ (* (relative-flex pl state) 2)
+	    (/ (asterisk-parties state) 4)
+	    (relative-freedom pl state))))))
 
-(defun meta-u (pl state)
-  (- (utility pl state) 
-     (utility (if (equal pl 'x) 'o 'x) state)))
+(defun careful-ev (pl state)
+  (let ((opp (opponent pl)) (moves (length (possible-moves pl state))))
+    (cond
+      ((eq moves 0) -999)
+      ((eq (length (possible-moves opp state)) 0) 999)
+      (t (+ (* (length (possible-moves pl state)) 4)
+	    (* (relative-freedom pl state) 2))))))
+
+(defun emergency? (pl state)
+  (let ((p-r (p-row (player pl state)))
+	(p-c (p-col (player pl state)))
+	(o-r (p-row (player (opponent pl) state)))
+	(o-c (p-col (player (opponent pl) state))))
+    (let ((enemy-close (or (eq (abs (- p-r o-r)) 1)
+			   (eq (abs (- p-c o-c)) 1)))
+	  (ast-prox (>= (adjacent-asterices p-r p-c state) 2))
+	  (few-moves (< (length (possible-moves pl state)) 10))
+	  (on-edge (or (eq p-r 8) (eq p-c 8) (eq p-r 1) (eq p-c 1))))
+    (or
+     (and enemy-close ast-prox)
+     (and enemy-close few-moves)
+     (and ast-prox few-moves)
+     (and enemy-close on-edge)))))
 
 ;check to see whether a node cannot have any more children
 (defun terminal? (node)
@@ -192,8 +261,10 @@
      new-states)))
 
 (defun max-value (pl node alpha beta depth)
+  (if (emergency? pl (state node))
+      (return-from max-value (careful-ev pl (state node))))
   (if (or (eq depth 0) (terminal? node))
-      (return-from max-value (utility pl (state node))))
+      (return-from max-value (casual-ev pl (state node))))
   (let ((value -999))
     (loop for s in (successors pl node) do
 	 (setf value (max value (min-value pl s alpha beta (- depth 1))))
@@ -202,20 +273,16 @@
     value))
 
 (defun min-value (pl node alpha beta depth)
+  (if (emergency? pl (state node))
+      (return-from min-value (careful-ev pl (state node))))
   (if (or (eq depth 0) (terminal? node))
-      (return-from min-value (utility pl (state node))))
+      (return-from min-value (casual-ev pl (state node))))
   (let ((value 999))
     (loop for s in (successors pl node) do
 	 (setf value (min value (max-value pl s alpha beta (- depth 1))))
 	 (if (<= value alpha) (return-from min-value value))
 	 (setf beta (min beta value)))
     value))
-
-(defun pair-up (l1 l2)
-  (cond
-    ((null l1) '())
-    ((null l2) '())
-    (t (cons `(,(car l1) = ,(car l2)) (pair-up (cdr l1) (cdr l2))))))q
 
 (defun best-move (pl state secs)
   (let ((kids (successors
@@ -245,9 +312,13 @@
 	(let ((opp (if (equal pl 'x) 'o 'x)) 
 	      (o-m (read))
 	      (n-state nil))
+	  (cond
+	      ((equal o-m 'stop) (return-from main 'done))
+	      ((not (listp o-m))
+		(format t "~%input must be in form (row column)~%")
+		(main pl state secs turn))) 
 	  (let ((row (car o-m)) (col (cadr o-m)))
 	    (cond
-	      ((equal o-m 'stop) (format t "done~%"))
 	      ((equal row 'rollback) ;rollback is requested
 	       (setf n-state (rollback state col))
 	       (format t "Board has been rolled back ~a times~%~a~%" 
